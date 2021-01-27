818eddb827c0e936c6b512e8b5d703150dd54c47
H4CK3R <H4CK3RD@python.org>
2021-01-27 10:33:40 +0100

    installation file

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ sudo pip3 install pikepdf
 sudo pip3 install rarfile
 
 sudo mkdir /usr/share/filedragon
-sudo cp src/main /usr/share/filedragon/
+sudo cp src/main.py /usr/share/filedragon/
 chmod +x filedragon
 sudo cp filedragon /bin/
